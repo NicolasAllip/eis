9224506df3d47152b490a533cc9010d417a6f996
Nicolas <allip.nicolas@gmail.com>
2015-09-07 23:46:19 -0300

    Tercer Test chopper

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -1,6 +1,12 @@
 class Chopper
 
+#chop(int,array) retorna el tamanho total del array sin el int que se 
+#pasa por parametro; si no hay elementos en el array retorna -1.
 	def chop(int,array)
-	  array.include?(int)?0:-1
+	  array.include?(int)?total_elements_within_int(int,array):-1
+	end
+	
+	def total_elements_within_int(int,array)
+	    array.size-array.count(int)
 	end
 end	
