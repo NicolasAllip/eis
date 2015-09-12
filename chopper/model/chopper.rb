0fecfdc3b9558463edba8834a6e2a8ed470adf5f
Nicolas <allip.nicolas@gmail.com>
2015-09-12 15:33:07 -0300

    Tercer test metodo sum()

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -2,6 +2,8 @@ class Chopper
 
 #chop(int,array) retorna el tamanho total del array sin el int que se 
 #pasa por parametro; si no hay elementos en el array retorna -1.
+	
+	
 	def chop(int,array)
 	    array.include?(int)?total_elements_within_int(int,array):-1
 	end
@@ -11,6 +13,15 @@ class Chopper
 	end
 	
 	def sum(array)
-	   array.empty? ? 'vacio':'uno'    
+		
+		array.empty? ? 'vacio' : sum_of_elements(array)
 	end
+
+	def sum_of_elements(array)
+	
+		total = array.inject{|sum,each| sum + each}
+		total ==1 ? 'uno' : 'cuatro'		
+	end
+	
+	
 end	
