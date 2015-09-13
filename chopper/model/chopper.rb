9b47c62f02b2155361e9557781fae0f871e3a7ea
Nicolas <allip.nicolas@gmail.com>
2015-09-12 21:49:49 -0300

    Test Quinto metodo sum()

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -5,27 +5,34 @@ class Chopper
 	
 	
 	def chop(int,array)
-	    array.include?(int)?total_elements_within_int(int,array):-1
+	  array.include?(int)?total_elements_within_int(int,array):-1
 	end
 	
 	def total_elements_within_int(int,array)
-	    array.size-array.count(int)
+	  array.size-array.count(int)
 	end
 	
 	def sum(array)
-		
-		array.empty? ? 'vacio' : sum_of_elements(array)
-	end
+          array.empty? ? 'vacio' : sum_of_elements(array)
+        end
 
-	def sum_of_elements(array)
-	
-	    sum_elements = array.inject{|sum,each| sum + each}
-	    total_of_digit = sum_elements.to_s.split('').map { |digit| digit.to_i }.size
-		
-	    (total_of_digit.equal?1) ? number_of_digit_eq_one(sum_elements) : 'uno,ocho'
-	end
+        def sum_of_elements(array)
 
-	def number_of_digit_eq_one(int)
-	    (int.equal?1) ? 'uno' : 'cuatro'
-	end
+          sum_elements = array.inject{|sum,each| sum + each}
+          digits = sum_elements.to_s.split('').map { |digit| digit.to_i }
+
+          ((digits.size).equal?3) ? 'demasiado grande' : number_of_digit_eq_one_or_two(sum_elements,digits) 
+        end
+
+        def number_of_digit_eq_one_or_two(int,digits)
+          ((digits.size).equal?1) ? number_of_digit_eq_one(int) : number_of_digit_eq_two()
+        end
+
+        def number_of_digit_eq_one(int)
+    	  (int.equal?1) ? 'uno' : 'cuatro'	
+        end
+
+        def number_of_digit_eq_two()
+    	  'uno,ocho'
+    end
 end	
