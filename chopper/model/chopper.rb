87375e70ff351ea4444da2b184219ae6ddccd1ca
Nicolas <allip.nicolas@gmail.com>
2015-09-17 21:30:41 -0300

    fix method sum() and chop(). aggregate additional test

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -1,17 +1,21 @@
 class Chopper
 
-#chop(int,array) retorna el tamanho total del array sin el int que se 
-#pasa por parametro; si no hay elementos en el array retorna -1.
+
+# chop(int,array) return the index(that is represented by int), 
+# inside the array. else return -1. 
+# In both methods the array not contains negative number.
 	
-	
-	def chop(int,array)
-	  array.include?(int)?total_elements_within_int(int,array):-1
+	def chop(number,array)
+		array.include?(number)? find_index(number,array):-1
 	end
-	
-	def total_elements_within_int(int,array)
-	  array.size-array.count(int)
+
+	def find_index(number,array)
+		array.index(number)
 	end
-	
+
+# sum(array), returns the string corresponding to each digit of the number (the sum of all the numbers of the array)
+# except the number of digits are greater than two
+
 	def sum(array)
 		array.empty? ? 'vacio' : sum_of_elements(array)
 	end
