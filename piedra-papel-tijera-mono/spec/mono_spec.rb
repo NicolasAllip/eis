84596a9fbe4575474b19b8157cbdc6a5b75f98c3
Nicolas <allip.nicolas@gmail.com>
2015-09-30 11:15:55 -0300

    se prueba que mono le gana a papel

---
+++ piedra-papel-tijera-mono/spec/mono_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec' 
+require_relative '../model/mono'
+require_relative '../model/piedra'
+require_relative '../model/tijera'
+require_relative '../model/papel'
+
+describe 'Mono' do
+
+	let(:piedra) { Piedra.new }
+	let(:tijera) { Tijera.new }
+	let(:mono) { Mono.new }
+	let(:papel) { Papel.new }
+
+	it 'mono le gana a papel' do
+	  expect(tijera.gana_contra_papel?()).to eq true
+	end	
+end
